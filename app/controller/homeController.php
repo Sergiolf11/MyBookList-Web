fb384ca413bf97ac62e44dca7c0fb443c13a5786
Sergiolf11 <sergiolf1121@gmail.com>
2022-06-13 16:59:27 +0200

    evitar ids inexistentes en url table

--- app/controller/homeController.php
+++ app/controller/homeController.php
@@ -53,22 +53,20 @@ function getAllFromUser(){
     $status = !empty($_GET['status']) ? $_GET['status'] : "0";
     if($status=="0"){
         $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid'  ORDER BY Estado,Saga,Num_Saga";  
-    }
-    if($status=="1"){
+    }else if($status=="1"){
         $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Reading' ORDER BY Saga,Num_Saga";  
-    }
-    if($status=="2"){
+    }else if($status=="2"){
         $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Completed' ORDER BY Saga,Num_Saga";  
-    }
-    if($status=="3"){
+    }else if($status=="3"){
         $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'On Hold' ORDER BY Saga,Num_Saga";  
-    }
-    if($status=="4"){
+    }else if($status=="4"){
         $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Dropped' ORDER BY Saga,Num_Saga";  
-    }
-    if($status=="5"){
+    }else if($status=="5"){
         $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Plan to Read' ORDER BY Saga,Num_Saga";  
-    }               
+    }else{
+        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid'  ORDER BY Estado,Saga,Num_Saga";  
+    }
+
     $result = mysqli_query($con, $sql);  
 
     if ($result->num_rows > 0) {
