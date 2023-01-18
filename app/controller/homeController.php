d21d8f61905865617f7f434dbe700713e54edffa
Sergio López <sergiolf1121@gmail.com>
2023-01-18 16:38:40 +0100

    sort by alphabet then number

--- app/controller/homeController.php
+++ app/controller/homeController.php
@@ -8,9 +8,9 @@ function getAll(){
     include '../../config/conexion.php'; 
     $search = !empty($_GET['search']) ? $_GET['search'] : "0";
     if($search=="0"){
-        $sql = "select * from libro  ORDER BY Saga,Num_Saga";  
+        $sql = "select * from libro  ORDER BY IF(Saga RLIKE '^[a-z]', 1, 2),Saga,Num_Saga";  
     }else{
-        $sql = "select * from libro where Titulo LIKE '%$search%' or Autor LIKE '%$search%' or Saga LIKE '%$search%' ORDER BY Saga,Num_Saga";  
+        $sql = "select * from libro where Titulo LIKE '%$search%' or Autor LIKE '%$search%' or Saga LIKE '%$search%' ORDER BY IF(Saga RLIKE '^[a-z]', 1, 2),Saga,Num_Saga";  
     }
     
     $result = $db->query($sql);  
@@ -52,19 +52,19 @@ function getAllFromUser(){
     include '../../config/conexion.php'; 
     $status = !empty($_GET['status']) ? $_GET['status'] : "0";
     if($status=="0"){
-        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid'  ORDER BY Saga,Num_Saga";  
+        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' ORDER BY IF(Saga RLIKE '^[a-z]', 1, 2),Saga,Num_Saga";  
     }else if($status=="1"){
-        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Reading' ORDER BY Saga,Num_Saga";  
+        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Reading' ORDER BY IF(Saga RLIKE '^[a-z]', 1, 2),Saga,Num_Saga";  
     }else if($status=="2"){
-        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Completed' ORDER BY Saga,Num_Saga";  
+        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Completed' ORDER BY IF(Saga RLIKE '^[a-z]', 1, 2),Saga,Num_Saga";  
     }else if($status=="3"){
-        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'On Hold' ORDER BY Saga,Num_Saga";  
+        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'On Hold' ORDER BY IF(Saga RLIKE '^[a-z]', 1, 2),Saga,Num_Saga";  
     }else if($status=="4"){
-        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Dropped' ORDER BY Saga,Num_Saga";  
+        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Dropped' ORDER BY IF(Saga RLIKE '^[a-z]', 1, 2),Saga,Num_Saga";  
     }else if($status=="5"){
-        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Plan to Read' ORDER BY Saga,Num_Saga";  
+        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid' and Estado = 'Plan to Read' ORDER BY IF(Saga RLIKE '^[a-z]', 1, 2),Saga,Num_Saga";  
     }else{
-        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid'  ORDER BY Saga,Num_Saga";  
+        $sql = "select * from usuario_libro ul join libro l on ul.Id_Libro=l.Id_Libro where Id_User = '$userid'  ORDER BY IF(Saga RLIKE '^[a-z]', 1, 2),Saga,Num_Saga";  
     }
 
     $result = $db->query($sql);  
