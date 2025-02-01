e6d89e539980e8a9d9806a9684c2c0af09714e66
Sergio López <sergiolf1121@gmail.com>
2025-02-01 13:53:10 +0100

    scanner si no isbn guardar isbn

--- app/controller/scannerController.php
+++ app/controller/scannerController.php
@@ -28,16 +28,33 @@ if (isset($_GET['ISBN'])) {
     $publishers = isset($book['publishers']) ? implode(", ", array_column($book['publishers'], 'name')) : 'Editorial desconocida';
     $excerpt = $book['excerpt']['value'] ?? 'No hay resumen disponible';
 
+    // Consulta en la base de datos si el libro ya existe por ISBN
+    $sqlisbn = "select * from libro where ISBN = '$isbn'";  
+    $resultisbn = $db->query($sqlisbn);
+
     // Consulta en la base de datos si el libro ya existe
     $sql = "select * from libro where LOWER(Titulo) = LOWER('$title') AND LOWER(Autor) = LOWER('$authors')";  
-    $result = $db->query($sql);  
+    $result = $db->query($sql); 
 
-    if ($result->num_rows > 0) {
+    if ($resultisbn->num_rows > 0) {
         $row = $result->fetch_assoc();
         $idlibro = $row['Id_Libro'];
         // Redirigir a libro.php con el idlibro
-        header("Location: http://mybooklist.rf.gd/app/view/libro.php?idlibro=$idlibro");
+        header("Location: ../view/libro.php?idlibro=$idlibro");
         exit;
+    }else if($result->num_rows > 0){
+        $row = $result->fetch_assoc();
+        $idlibro = $row['Id_Libro'];
+        $sqlupdate = "UPDATE libro SET ISBN='$isbn' WHERE Id_Libro = ".$idlibro."";
+        $resultupdate = $db->query($sqlupdate);  
+        if($resultupdate){  
+            header("Location: ../view/libro.php?idlibro=$idlibro");
+            exit;
+        }  
+        else{  
+            echo "Error: ".$sql."<br>".$mysql_error($db);  
+        }   
+        // Redirigir a libro.php con el idlibro
     } else {
         // Redirigir a scanner.php pasándole el ISBN
         header("Location: ../view/scanner.php?ISBN=$isbn");
